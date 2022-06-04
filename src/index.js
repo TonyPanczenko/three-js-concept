7e4b83888df64a62365cd1c4f0bcda28d78a4950
Tony Panczenko <tony.panczenko@gmail.com>
2022-06-04 22:26:52 +0100

    init scene, camera, renderer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import { WebGLRenderer } from 'three';
+
+import scene from './scene/scene';
+import camera from 'camera';
+
+const renderer = new WebGLRenderer();
+renderer.setSize(innerWidth, innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
