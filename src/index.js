d4e47d0f61a2414c3b59f090c2091a52cf775cb1
Tony Panczenko <tony.panczenko@gmail.com>
2022-06-05 22:22:42 +0100

    update renderer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { WebGLRenderer } from 'three';
 import scene from './scene/scene';
 import camera from './camera';
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({
+  antialias: true,
+});
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
